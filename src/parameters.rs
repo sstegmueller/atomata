5111f682a772f97e4f145c03e1f42e6dd35a9a91
Stefan Stegmueller <stefan.steg@gmail.com>
2024-08-16 08:45:08 +0200

    add(#19): parameter space implementation

--- src/parameters.rs
+++ src/parameters.rs
@@ -105,40 +105,72 @@ impl Parameters {
     }
 
     pub fn parameter_space() -> Vec<Self> {
-        vec![Parameters {
-            amount: 10,
-            border: 200.0,
-            friction: 0.005,
-            timestep: 0.0002,
-            gravity_constant: 1.0,
-            particle_parameters: vec![
-                ParticleParameters {
-                    id: None,
-                    mass: 3.0,
-                    index: 0,
-                },
-                ParticleParameters {
-                    id: None,
-                    mass: 250.0,
-                    index: 1,
-                },
-                ParticleParameters {
-                    id: None,
-                    mass: 1000.0,
-                    index: 2,
-                },
-            ],
-            interactions: vec![
-                InteractionType::Repulsion,  // 0 <-> 0
-                InteractionType::Attraction, // 1 <-> 0
-                InteractionType::Attraction, // 2 <-> 0
-                InteractionType::Repulsion,  // 1 <-> 1
-                InteractionType::Attraction, // 1 <-> 2
-                InteractionType::Neutral,    // 2 <-> 2
-            ],
-            max_velocity: 20000.0,
-            bucket_size: 10.0,
-        }]
+        let mut parameter_space = vec![];
+
+        let amounts = vec![10, 20, 30, 100, 500, 1000];
+        let borders = vec![200.0, 400.0, 600.0, 2000.0];
+        let frictions = vec![0.0, 0.005, 0.01, 2.0];
+        let timesteps = vec![0.0002, 0.0004, 0.0006];
+        let gravity_constants = vec![0.5, 1.0, 2.0, 3.0];
+        let max_velocities = vec![20000.0, 40000.0, 60000.0];
+        let bucket_sizes = vec![2.0, 5.0, 10.0, 20.0, 30.0];
+
+        for amount in amounts {
+            for border in &borders {
+                for friction in &frictions {
+                    for timestep in &timesteps {
+                        for gravity_constant in &gravity_constants {
+                            for max_velocity in &max_velocities {
+                                for bucket_size in &bucket_sizes {
+                                    let particle_parameters = vec![
+                                        ParticleParameters {
+                                            id: None,
+                                            mass: 3.0,
+                                            index: 0,
+                                        },
+                                        ParticleParameters {
+                                            id: None,
+                                            mass: 250.0,
+                                            index: 1,
+                                        },
+                                        ParticleParameters {
+                                            id: None,
+                                            mass: 1000.0,
+                                            index: 2,
+                                        },
+                                    ];
+
+                                    let interactions = vec![
+                                        InteractionType::Repulsion,  // 0 <-> 0
+                                        InteractionType::Attraction, // 1 <-> 0
+                                        InteractionType::Attraction, // 2 <-> 0
+                                        InteractionType::Repulsion,  // 1 <-> 1
+                                        InteractionType::Attraction, // 1 <-> 2
+                                        InteractionType::Neutral,    // 2 <-> 2
+                                    ];
+
+                                    let parameters = Parameters {
+                                        amount,
+                                        border: *border,
+                                        friction: *friction,
+                                        timestep: *timestep,
+                                        gravity_constant: *gravity_constant,
+                                        particle_parameters,
+                                        interactions,
+                                        max_velocity: *max_velocity,
+                                        bucket_size: *bucket_size,
+                                    };
+
+                                    parameter_space.push(parameters);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        parameter_space
     }
 }
 
