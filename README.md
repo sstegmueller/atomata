25727e965f3e50f82f83720c706b30184e2f0f6e
Stefan Stegmueller <stefan.steg@gmail.com>
2024-08-18 17:12:51 +0200

    feat(#10): init jupyter environemnt

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # atomata
+
+## Setup
+- Enable nix-flake:
+```
+nix develop
+```
+
+- Run the application (use '-s' flag for search mode):
+```
+cargo run
+```
+
+- Build for wasm
+```
+cd web && sh build.sh
+```
+
+- Analyze search mode results
+```
+jupyter-lab
+```
--- flake.nix
+++ flake.nix
@@ -33,20 +33,33 @@
               extensions = [ "rust-src" ];
               targets = [ "wasm32-unknown-unknown" ];
             })
+            # Include Python setup from shell.nix
+            (python3.withPackages (p: with p; [
+              virtualenv
+            ]))
           ];
 
+          # Combine LD_LIBRARY_PATH from shell.nix and existing setup
+          LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [
+            pkgs.libGL
+            pkgs.libxkbcommon
+            pkgs.wayland
+            pkgs.xorg.libX11
+            pkgs.xorg.libXcursor
+            pkgs.xorg.libXi
+            pkgs.xorg.libXrandr
+            pkgs.stdenv.cc.cc.lib
+          ];
+
+          # Combine shellHook from shell.nix and existing setup
           shellHook = ''
             export PATH=$PATH:~/.cargo/bin
             export RUST_BACKTRACE=1
-            export LD_LIBRARY_PATH=${pkgs.lib.makeLibraryPath [
-              pkgs.libGL
-              pkgs.libxkbcommon
-              pkgs.wayland
-              pkgs.xorg.libX11
-              pkgs.xorg.libXcursor
-              pkgs.xorg.libXi
-              pkgs.xorg.libXrandr
-            ]}
+
+            python -m venv venv
+            . venv/bin/activate
+            pip install -r requirements.txt
+            ipython kernel install --name "local-venv" --user
           '';
 
           RUST_SRC_PATH = "${pkgs.rustPlatform.rustLibSrc}";
